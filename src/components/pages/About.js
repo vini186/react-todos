9316aa7584815e7953acad4ea9f07b0879235771
vini186 <kallavineeta@gmail.com>
2022-03-16 21:02:03 +0530

    initial

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+ function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is the TodoList app v1.0.0. It is a part of React Crash Course</p> 
+    </React.Fragment>
+  )
+}
+
+
+export default About
